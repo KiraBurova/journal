5d4b0039b42e4f74896e6c12b789d53098347ebc
Kira Burova <burovakira11@gmail.com>
2022-01-22 15:22:37 +0300

    added webpack config, eslint, starter readme and refs

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # journal
+
+- sigin signup design [here](https://dribbble.com/shots/16705889-Login-Sign-up-AW-Universal-Page)
+
+Steps:
+
+- Create UI kit
+- Cover UI kit with tests
+
+Instruments:
+
+- React
+- Typescript
+- Jest / React-testing-library
+- Images: Unsplash
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'index.bundle.js',
+  },
+  devServer: {
+    port: 3000,
+    liveReload: true,
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' }), new MiniCssExtractPlugin()],
+}
