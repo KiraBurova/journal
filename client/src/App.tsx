bd688cb16a81a05b682fd6b1ffa54bcefbc5cec2
Kira Burova <burovakira11@gmail.com>
2022-01-29 11:36:02 +0300

    added graphql packages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,12 @@
 import React, { lazy } from 'react'
 import { Route, Routes } from 'react-router-dom'
 
-import Button from './ui/button'
-
 const RegistrationPage = lazy(() => import('./components/RegistrationForm'))
 
 const App = () => (
-    <>
-        <Button onClick={() => console.log(1)}>asasdas</Button>
-        <Routes>
-            <Route path="/" element={<RegistrationPage />} />
-        </Routes>
-    </>
+    <Routes>
+        <Route path="/" element={<RegistrationPage />} />
+    </Routes>
 )
 
 export default App
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,7 @@
 import React, { Suspense } from 'react'
 import * as ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 import App from './App'
 
 import 'normalize.css'
@@ -8,13 +9,20 @@ import 'normalize.css'
 import './styles/vars.scss'
 import './App.scss'
 
+const client = new ApolloClient({
+    uri: 'http://localhost:4000',
+    cache: new InMemoryCache(),
+})
+
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <Suspense fallback="Loading...">
-                <App />
-            </Suspense>
-        </BrowserRouter>
+        <ApolloProvider client={client}>
+            <BrowserRouter>
+                <Suspense fallback="Loading...">
+                    <App />
+                </Suspense>
+            </BrowserRouter>
+        </ApolloProvider>
     </React.StrictMode>,
     document.getElementById('root'),
 )
