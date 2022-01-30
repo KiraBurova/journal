0abc6a8497b5c6ef1e409c4c03cac622a4130f8b
Kira Burova <burovakira11@gmail.com>
2022-01-30 15:31:20 +0300

    setting up apollo client, added basic files and packages for the server

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,5 +1,8 @@
 import * as React from 'react'
 
+import { ApolloProvider } from '@apollo/client'
+import client from '../lib/apolloClient'
+
 import '../styles/globals.scss'
 import '../styles/vars.scss'
 import '../styles/fonts.scss'
@@ -9,4 +12,8 @@ interface IApp {
     pageProps: unknown
 }
 
-export default ({ Component, pageProps }: IApp) => <Component {...pageProps} />
+export default ({ Component, pageProps }: IApp) => (
+    <ApolloProvider client={client}>
+        <Component {...pageProps} />
+    </ApolloProvider>
+)
